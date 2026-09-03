44512c114b95ed3d443ec9dd26161de30a3c9415
Ankita Gusain <ankitagusain0129@gmail.com>
2026-09-03 11:50:46 +0530

    3 sept

--- string/creatingString.js
+++ string/creatingString.js
@@ -4,3 +4,7 @@ let c=`String 3`;
 console.log(`with double quotes ${a}`);
 console.log(`with single quotes ${b}`);
 console.log(`with template literal ${c}`);
+//new line
+console.log(`with template literal\n ${c}`);
+//tab
+console.log(`with template literal\n \t ${c}`)
